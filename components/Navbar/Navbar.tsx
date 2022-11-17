3d9e550f76f741affcc69cafb792f1c624c055e9
Cyberpunk <ruancosta9797@gmail.com>
2022-11-17 14:00:27 -0300

    Subindo projeto

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import Link from "next/link"
+import styles from "./Navbar.module.css"
+import { useRouter } from "next/router"
+
+
+
+export default function Navbar(){
+
+    const router = useRouter()
+
+    return(
+    <nav className={styles.navbar}>
+        <div className={styles.container}>
+            <span>MCloud</span>
+            <div className={styles.divider}></div>
+            <ul>
+                <li className={router.pathname === '/' ? styles.active : ''}>
+                    <Link href="/">Home</Link>
+                </li>
+                <li className={router.pathname === '/unity' ? styles.active : ''}>
+                    <Link href="/unity">Unidades</Link>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    )
+}
---
+++ pages/unity.tsx
@@ -0,0 +1,8 @@
+
+export default function Unity(){
+    return(
+        <div>
+            <h1>Minha página</h1>
+        </div>
+    )
+}
