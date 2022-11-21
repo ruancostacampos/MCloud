57a7c151adb52dcb8484db1d748b15a5f4fef6ba
Cyberpunk <ruancosta9797@gmail.com>
2022-11-21 12:56:31 -0300

    Criando card

---
+++ components/Card/Card.tsx
@@ -0,0 +1,42 @@
+import styles  from "./Card.module.css"
+import Icon from "../../public/images/server.svg"
+import MegaIcon from "../../public/images/mega.svg"
+import AnyDeskIcon from "../../public/images/anydesk.svg"
+import Image from "next/image"
+import internal from "stream"
+
+export interface Props {
+    unityName: string;
+    sync: boolean;
+    anydeskCount: number;
+    cnes: string;
+}
+
+export default function Card(props: Props){
+
+    return (
+        <div className={styles.container}>
+            <div>
+                <div className={styles.anydesk}>
+                    <Image width={20} src={AnyDeskIcon}  alt="Anydesk Icon"/>
+                    <span>{props.anydeskCount}</span>
+                </div>
+                <h2>{props.unityName}</h2>
+                <div className={styles.mega}>
+                    <Image width={20} src={MegaIcon} alt="Mega icon"/>
+                    {props.sync ? <p>A unidade enviou backup ontem.</p> : 
+                                <p>A unidade não enviou arquivos na última noite.</p>}
+                </div>
+                <p>CNES: {props.cnes}</p>
+                <div className={styles.wrapButton}>
+                    <Image
+                        src={Icon} 
+                        alt="Icon" 
+                    />
+                    <button>Detalhes</button>
+                </div>
+            </div>
+        </div>
+    )
+ 
+}
